739976eda9f75d690350671b64c068072988909c
“3mpt” <2807213137@qq.com>
2025-08-14 00:20:20 +0800

    feat: dp

--- 灵茶山艾府/[17]动态规划入门/[213]打家劫舍 II.js
+++ 灵茶山艾府/[17]动态规划入门/[213]打家劫舍 II.js
@@ -2,18 +2,47 @@
  * @param {number[]} nums
  * @return {number}
  */
+// export let rob = function (nums) {
+//   let n = nums.length
+//   return Math.max(nums[0] + rob1(nums.slice(2, n - 1)), rob1(nums.slice(1)))
+// }
+// function rob1(nums) {
+//   let n = nums.length
+//   let cache = Array(n).fill(-1)
+//   function fn(i) {
+//     if (i < 0) return 0
+
+//     if (cache[i] !== -1) return cache[i]
+//     return (cache[i] = Math.max(fn(i - 1), fn(i - 2) + nums[i]))
+//   }
+//   return fn(n - 1)
+// }
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 export let rob = function (nums) {
-  let n = nums.length
-  return Math.max(nums[0] + rob1(nums.slice(2, n - 1)), rob1(nums.slice(1)))
+  //  总共有n个房子,最后一个房子下标是n-1
+  const n = nums.length
+  return Math.max(
+    rob1(nums.slice(2, n - 1)) + nums[0], //偷第1间 就不能偷最后一间 子问题就是第3间开始, 然后到n-2 slice包括 start，不包括 end
+    rob1(nums.slice(1)) // 不偷第一间 就是从第2件开始偷
+  )
 }
 function rob1(nums) {
-  let n = nums.length
-  let cache = Array(n).fill(-1)
+  const n = nums.length
+  const cache = new Map()
   function fn(i) {
+    // 没有房子，收益是 0
     if (i < 0) return 0
+    if (cache.has(i)) return cache.get(i)
 
-    if (cache[i] !== -1) return cache[i]
-    return (cache[i] = Math.max(fn(i - 1), fn(i - 2) + nums[i]))
+    const res = Math.max(
+      fn(i - 2) + nums[i], // 偷当前这间,不能偷i-1 就应该从i-2 开始偷
+      fn(i - 1) // 不偷当前这间 就从i-1 继续递归
+    )
+    cache.set(i, res)
+    return res
   }
   return fn(n - 1)
 }
--- 灵茶山艾府/[17]动态规划入门/[740]删除并获得点数.js
+++ 灵茶山艾府/[17]动态规划入门/[740]删除并获得点数.js
@@ -21,3 +21,35 @@ export let deleteAndEarn = function (nums) {
   }
   return dp1
 }
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+export const deleteAndEarn1 = function (nums) {
+  let max = Math.max(...nums)
+  let arr = Array(max + 1).fill(0)
+
+  for (let num of nums) {
+    arr[num] += num
+  }
+  let cache = new Map()
+  // i是一个数 map.get(i) 是这个数的钱数
+  function fn(i) {
+    if (i < 0) return 0
+    // if (arr[i] === 0) return fn(i - 1)
+
+    if (cache.has(i)) return cache.get(i)
+    const res = Math.max(
+      // 选这个数 则需要从
+      fn(i - 2) + arr[i],
+      // 不选
+      fn(i - 1)
+    )
+    cache.set(i, res)
+
+    return res
+  }
+
+  return fn(max)
+}
